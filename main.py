981b89fcb0253ee39f21316a07b0e0ec5b8d22a4
DWitol <Devon_Witol@live.com>
2021-02-26 15:56:23 -0500

    Lots of bug Fixing.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def initialGameState():
     Gameboard[1][3 ] = 1
  ##returns a 2d array with the cells directly surrounding the home coordinates
 def surroundingState(n,x = Gameboard,d=1):
-    return x[n[0]-d:n[0]+d+1,n[1]-d:n[1]+d+1]
+    return x[max(0,n[0]-d):min(boardSize,n[0]+d+1),max(0,n[1]-d):min(boardSize,n[1]+d+1)]
 
 
 
@@ -22,23 +22,35 @@ def surroundingState(n,x = Gameboard,d=1):
 if __name__ == '__main__':
     initialGameState()
     print(Gameboard)
-    simulations = 10
+    simulations = 1
     count = 0
     finalNeighbour = Gameboard[0:0+3, 0:0+3]
-    while(count <= simulations):
+    births = 0
+    deathByIsolation = 0
+    deathByOvercrowding = 0
+    survival = 0
+    while(count < simulations):
         nextboard = np.zeros((boardSize, boardSize))
         for i in range(len(Gameboard)-1):
             for j in range(len(Gameboard)-1):
+                neighbours = surroundingState((i,j))
                 #Birth rule
-                neighbours = Gameboard[i:i+3, j:j+3]
-                neighbours = np.sum(Gameboard[i:i+3, j:j+3] == 1)
-                if np.sum(Gameboard[i:i+3, j:j+3] == 1) == 3 and Gameboard[i+1,j+1] == 0: nextboard[i+1,j+1] = 1
+                if np.sum(neighbours == 1) == 3 and Gameboard[i,j] == 0:
+                    births = births +1
+                    nextboard[i,j] = 1
                 #Death by isolation
-                if np.sum(Gameboard[i:i+3, j:j+3] == 1) <= 1 and Gameboard[i+1,j+1] == 1: nextboard[i+1,j+1] = 0
+                if np.sum(neighbours == 1) <= 1 and Gameboard[i,j] == 1:
+                    deathByIsolation = deathByIsolation + 1
+                    nextboard[i,j] = 0
                 #Death by overcrowding
-                if np.sum(Gameboard[i:i+3, j:j+3] == 1) <= 4 and Gameboard[i+1,j+1] == 1: nextboard[i+1,j+1] = 0
-                #survival
-                if (np.sum(Gameboard[i:i + 3, j:j + 3] == 1) == 2 or np.sum(Gameboard[i:i + 3, j:j + 3] == 1) == 3) and Gameboard[i+1,j+1] == 1: nextboard[i+1,j+1] = 1
+                if np.sum(neighbours == 1) >= 4 and Gameboard[i,j] == 1:
+                    deathByOvercrowding = deathByOvercrowding + 1
+                    nextboard[i,j] = 0
+                #Survival
+                if (np.sum(neighbours == 1) == 2 or np.sum(neighbours == 1) == 3) and Gameboard[i+1,j+1] == 1:
+                    survival = survival+1
+                    nextboard[i+1,j+1] = 1
+        print(births,deathByIsolation,deathByOvercrowding,survival)
 
         Gameboard = nextboard
         print(Gameboard)
