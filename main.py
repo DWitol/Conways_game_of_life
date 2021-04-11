652a2f8a2d869f0853ecf68baa895c4825649adb
DWitol <Devon_Witol@live.com>
2021-04-11 19:29:51 -0400

    code Cleanup

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
-
-
-
 boardSize = 20
 import numpy as np
-from future.moves import tkinter as tk
 import pygame
-from pygame.locals import *
 import time
 
 true = True
 false = False
-Live = (50,50,50)
-Dead = (255,255,255)
+Live = (50, 50, 50)
+Dead = (255, 255, 255)
 
 
-
-
-
-def initialGameState(Gameboard ):
-
-    Gameboard[0][2] = 1
-    Gameboard[1][3] = 1
-    Gameboard[2][1] = 1
-    Gameboard[2][2] = 1
-    Gameboard[2][3] = 1
-    return Gameboard
+def initialGameState(gameBoard):
+    gameBoard[0][2] = 1
+    gameBoard[1][3] = 1
+    gameBoard[2][1] = 1
+    gameBoard[2][2] = 1
+    gameBoard[2][3] = 1
+    return gameBoard
 
 
 # returns a 2d array with the cells directly surrounding the home coordinates
@@ -32,34 +23,32 @@ def initialGameState(Gameboard ):
 # this currently only works with d=1 due to padding. Generalization can be implemented later
 def surroundingState(n, x, d=1):
     value = x[max(0, n[0] - d):min(boardSize, n[0] + d + 1), max(0, n[1] - d):min(boardSize, n[1] + d + 1)]
-    #if on far left edge
-    if n[1] == 0 :
-        value = np.append(np.zeros((value.shape[0],1)),value,axis = 1)
+    # if on far left edge
+    if n[1] == 0:
+        value = np.append(np.zeros((value.shape[0], 1)), value, axis=1)
     if n[0] == 0:
-        value = np.append(np.zeros((1,3)),value,axis = 0)
+        value = np.append(np.zeros((1, 3)), value, axis=0)
         ## TODO These next two need to be checked. There is something wrong with them
     if n[0] == boardSize:
-        value = np.append(value,np.zeroes((2,1),axis=1))
-    if n[1]== boardSize:
-        value = np.append(value,np.zeros((3,1)),axis = 0)
-
+        value = np.append(value, np.zeroes((2, 1), axis=1))
+    if n[1] == boardSize:
+        value = np.append(value, np.zeros((3, 1)), axis=0)
 
     return value
 
 
 if __name__ == '__main__':
-#todo add click to add cell functionality
-#todo add pause button
+    # todo add click to add cell functionality
+    # todo add pause button
     Gameboard = np.zeros((boardSize, boardSize))
     Gameboard = initialGameState(Gameboard)
     print(Gameboard)
     pygame.init()
     screen = pygame.display.set_mode((640, 240 * 2))
-    pygame.draw.rect(screen,(255,255,0),[500,520,500,520])
+    pygame.draw.rect(screen, (255, 255, 0), [500, 520, 500, 520])
     screen.fill((0, 0, 0))
     x = 0
     # cell = Rect()
-
 
     running = true
     while running:
@@ -75,19 +64,18 @@ if __name__ == '__main__':
                     cellColour = Dead
                 else:
                     cellColour = Live
-                pygame.draw.rect(screen, cellColour, (20 + x * 22, 20 + y * 22, 20, 20),border_radius=1)
+                pygame.draw.rect(screen, cellColour, (20 + x * 22, 20 + y * 22, 20, 20), border_radius=1)
                 y += 1
             x += 1
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT: running = false
 
-
         nextboard = np.zeros((boardSize, boardSize))
 
         for i in range(len(Gameboard) - 1):
             for j in range(len(Gameboard) - 1):
-                neighbours = np.sum(surroundingState((i, j),Gameboard))
+                neighbours = np.sum(surroundingState((i, j), Gameboard))
 
                 # Birth rule
                 if neighbours == 3 and Gameboard[i, j] == 0:
@@ -98,7 +86,7 @@ if __name__ == '__main__':
                     nextboard[i, j] = 0
 
                 # Death by overcrowding
-                elif neighbours >= 5 and Gameboard[i,j] == 1:
+                elif neighbours >= 5 and Gameboard[i, j] == 1:
                     nextboard[i, j] = 0
 
                 # Survival
@@ -107,7 +95,6 @@ if __name__ == '__main__':
 
         Gameboard = nextboard
         print(pygame.event.get())
-      #  print(Gameboard)
+        #  print(Gameboard)
         print()
         time.sleep(max(0.4 - (time.time() - start), 0))
-
